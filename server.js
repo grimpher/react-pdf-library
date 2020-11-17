33151ea9a62e320884c98c38477bdffbfdfa774c
Grimpher <komarnicki115@gmail.com>
2020-11-17 14:36:46 +0100

    fix client path

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/api', apiRouter)
 
 app.use(express.static(path.join(__dirname, 'public/client')))
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public/client', 'index.html'))
 })
 
 // MONGODB SETUP
